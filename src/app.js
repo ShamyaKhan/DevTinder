51be2ed3cb8886b1f3e263c97736d3e97d6d79a8
Shamya Khan <md.shamiulhaque127@gmail.com>
2025-12-01 12:18:23 +0600

    Data Sanitization & Schema Validation

--- src/app.js
+++ src/app.js
@@ -3,18 +3,77 @@ const { connectDB } = require("./config/database");
 const app = express();
 const { User } = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Shahrukh",
-    lastName: "Khan",
-    emailId: "shahrukh@gmail.com",
-    password: "Shahrukh@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User created successfully");
   } catch (err) {
-    res.status(400).send("Some error occurred ", err.message);
+    res.status(400).send("Some error occurred " + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find({ emailId: req.body.emailId });
+    if (users.length === 0) {
+      res.status(404).send("No user found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const data = req.body;
+  const userId = req.params?.userId;
+
+  try {
+    const allowedUpdates = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      allowedUpdates.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Cannot add more than 10 skills");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong " + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,54 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, minLength: 3 },
+    lastName: { type: String, required: true },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Please enter a strong password");
+        }
+      },
+    },
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo URL");
+        }
+      },
+    },
+    about: { type: String, default: "This is default about" },
+    skills: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
