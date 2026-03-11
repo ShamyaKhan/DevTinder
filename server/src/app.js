a236e73346db14df96ce46a54e04272ab7b8cf3d
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-03-11 14:49:54 +0600

    fixed env variable

--- server/src/app.js
+++ server/src/app.js
@@ -3,21 +3,21 @@ const { connectDB } = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use(express.json());
-app.use(cookieParser());
-
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const { CLIENT_BASE_URL } = require("./utils/constants");
+
+app.use(
+  cors({
+    origin: CLIENT_BASE_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
---
+++ server/src/utils/constants.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL;
+
+module.exports = { CLIENT_BASE_URL };
